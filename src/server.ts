e924e5cc6041ef733ac16b945d3fe54a8a3435b2
Leo <leon_fritas@hotmail.com>
2023-08-21 21:24:11 -0400

    configurações express

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+
+
+const port = 3000;
+
+const app = express();
+
+// GET, POST, PUT, PATCH, DELETE
+// PARA BUSCAR LISTA - GET
+
+app.get("/movies", (req, res) => {
+    res.send("Listagem de filmes");
+
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor em execução na porta ${port}`);
+});
