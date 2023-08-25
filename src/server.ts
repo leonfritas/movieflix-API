2b38ceb6f7b8607ff52262c3f76140093c0e95c9
Leo <leon_fritas@hotmail.com>
2023-08-24 22:33:46 -0400

    adicionado filtro de filmes por gênero.

--- src/server.ts
+++ src/server.ts
@@ -88,33 +88,59 @@ app.put("/movies/:id", async (req, res) => {
 
         });
     } catch (error) {
-        return res.status(500).send({message: "Falha ao atualizar o registro do filme."});
+        return res.status(500).send({ message: "Falha ao atualizar o registro do filme." });
     }
     res.status(200).send();
 });
 
 
-app.delete("/movies/:id", async (req,res) => {
+app.delete("/movies/:id", async (req, res) => {
     const id = Number(req.params.id);
 
-    try{
+    try {
         const movie = await prisma.movie.findUnique({
             where: { id }
         });
-        if (!movie){
-            return res.status(404).send({message:"Esta filme não está cadastrado."});
+        if (!movie) {
+            return res.status(404).send({ message: "Esta filme não está cadastrado." });
         }
 
         await prisma.movie.delete({
             where: { id }
         });
-    }catch(error){
-        return res.status(500).send({message: "Não foi possível remover o filme."});
+    } catch (error) {
+        return res.status(500).send({ message: "Não foi possível remover o filme." });
     }
 
     res.status(200).send();
 
 });
+
+
+app.get("/movies/:genreName", async (req, res) => {
+
+    try {
+        const moviesFilteredByGenreName = await prisma.movie.findMany({
+            include: {
+                genres: true,
+                languages: true
+            },
+            where: {
+                genres: {
+                    name: {
+                        equals: req.params.genreName,
+                        mode: "insensitive"
+                    }
+                }
+            }
+        });
+        res.status(200).send(moviesFilteredByGenreName);
+    } catch (erroe) {
+        res.status(500).send({message:"Gênero não encontrado"});
+    }
+    
+});
+
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
